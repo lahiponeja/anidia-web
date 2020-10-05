7c2886172f45ab4b3673eca8983a3479344802c5
Clara Morrondo <clara.morrondo@the-cocktail.com>
2020-10-05 13:28:41 +0200

    first structure added

---
+++ theme/anidia-fragments/src/anidia-collection/stepper-module/main.js
@@ -0,0 +1,22 @@
+console.group('stepper-module');
+console.log('fragmentElement', fragmentElement);
+console.groupEnd();
+
+
+let height = -1;
+
+// document.querySelectorAll('.an-stepper__tab__container').forEach();
+// (function($, document) {
+
+//   // get tallest tab__content element
+//   let height = -1;
+
+// $('.tab__content').each(function() {
+//   height = height > $(this).outerHeight() ? height : $(this).outerHeight();
+//      $(this).css('position', 'absolute');
+// });
+
+//   // set height of tabs + top offset
+// $('[data-tabs]').css('min-height', height + 40 + 'px');
+
+// }(jQuery, document));
