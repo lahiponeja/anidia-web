554d583259bd3b5e26f2859e6ca15d269f4a1625
David J. Brenes <david.brenes@the-cocktail.com>
2020-10-16 19:49:42 +0200

    Added Salesforce Services class to fetch info from CRM

---
+++ theme/anidia-portlets/sample-vue-portlet/impl/src/main/java/com/liferay/sampleVue/internal/services/SalesforceService.java
@@ -0,0 +1,136 @@
+package com.liferay.sampleVue.internal.services;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.sampleVue.dto.v1_0.Address;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+
+public class SalesforceService {
+
+	static String SALESFORCE_TOKEN_URL = System.getenv().get("SALESFORCE_TOKEN_URL");
+	static String SALESFORCE_ADRESSES_URL = System.getenv().get("SALESFORCE_ADDRESSES_URL");
+	static String SALESFORCE_PASSWORD = System.getenv().get("SALESFORCE_PASSWORD");
+	static String SALESFORCE_CLIENT_SECRET = System.getenv().get("SALESFORCE_CLIENT_SECRET");
+	static String SALESFORCE_CLIENT_ID = System.getenv().get("SALESFORCE_CLIENT_ID");
+	static String SALESFORCE_USERNAME = System.getenv().get("SALESFORCE_USERNAME");
+
+	public List<Address> getAddresses(String municipalityId, String postalCode) {
+
+		List<Address> addresses = new ArrayList<Address>();
+		String token = this.getSalesforceToken();
+
+		StringBuilder urlBuilder = new StringBuilder();
+		urlBuilder.append(SALESFORCE_ADRESSES_URL);
+		urlBuilder.append("?");
+		urlBuilder.append("municipio_ine=");
+		urlBuilder.append(municipalityId);
+		urlBuilder.append("&codigo_postal=");
+		urlBuilder.append(postalCode);
+
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder().
+			uri(URI.create(urlBuilder.toString())).
+			header("Authorization", "Bearer " + token).
+			GET().
+			build();
+
+		HttpResponse<String> response = null;
+		JSONArray responseJson;
+
+		System.out.println("Requesting addresses to " + urlBuilder.toString());
+
+		try {
+			response = client.send(request, HttpResponse.BodyHandlers.ofString());
+			responseJson = new JSONArray(response.body());
+//			responseJson = new JSONArray("[{\"attributes\":{\"type\":\"AggregateResult\"},\"Tipo_de_via__c\":\"CL\",\"Nombre_de_via__c\":\"TEJEDORES\"},{\"attributes\":{\"type\":\"AggregateResult\"},\"Tipo_de_via__c\":\"CL\",\"Nombre_de_via__c\":\"TEJEDORES 2\"}]");
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+			return addresses;
+		} catch (JSONException e) {
+			if(response != null) {
+				System.out.println("Salesforce response: " + response.body());
+			}
+			e.printStackTrace();
+			return addresses;
+		}
+
+		// The response format is [{"attributes":{"type":"AggregateResult"},"Tipo_de_via__c":"CL","Nombre_de_via__c":"TEJEDORES"}]
+		for (int i = 0; i < responseJson.length(); i++) {
+			JSONObject addressJson = null;
+			try {
+				addressJson = responseJson.getJSONObject(i);
+				Address address = new Address();
+				address.setKind(addressJson.getString("Tipo_de_via__c"));
+				address.setName(addressJson.getString("Nombre_de_via__c"));
+				addresses.add(address);
+			} catch (JSONException e) {
+				System.out.println("Salesforce response: " + response.body());
+				if(addressJson != null) {
+					System.out.println("Json Object with error: " + addressJson.toString());
+				}
+				e.printStackTrace();
+			}
+		}
+		return addresses;
+	}
+
+	private String getSalesforceToken() {
+
+		StringBuilder urlBuilder = new StringBuilder();
+		urlBuilder.append(SALESFORCE_TOKEN_URL);
+		urlBuilder.append("?");
+		urlBuilder.append("grant_type=password");
+		urlBuilder.append("&client_id=");
+		urlBuilder.append(SALESFORCE_CLIENT_ID);
+		urlBuilder.append("&client_secret=");
+		urlBuilder.append(SALESFORCE_CLIENT_SECRET);
+		urlBuilder.append("&username=");
+		urlBuilder.append(SALESFORCE_USERNAME);
+		urlBuilder.append("&password=");
+		urlBuilder.append(SALESFORCE_PASSWORD);
+
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder().
+			uri(URI.create(urlBuilder.toString())).
+			POST(HttpRequest.BodyPublishers.noBody()).
+			build();
+
+		HttpResponse<String> response;
+		try {
+			response = client.send(request, HttpResponse.BodyHandlers.ofString());
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+	  JSONObject json;
+		try {
+			json = new JSONObject(response.body());
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		try {
+			return json.getString("access_token");
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
