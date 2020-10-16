dcebf1dd087e2428abfcce5e740256bcb8e38d2d
David J. Brenes <david.brenes@the-cocktail.com>
2020-10-16 20:26:33 +0200

    Implemented the actual estates service by using the salesforce service

--- theme/anidia-portlets/sample-vue-portlet/impl/src/main/java/com/liferay/sampleVue/internal/resource/v1_0/EstateResourceImpl.java
+++ theme/anidia-portlets/sample-vue-portlet/impl/src/main/java/com/liferay/sampleVue/internal/resource/v1_0/EstateResourceImpl.java
@@ -1,9 +1,25 @@
 package com.liferay.sampleVue.internal.resource.v1_0;
 
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.sampleVue.internal.services.SalesforceService;
 import com.liferay.sampleVue.resource.v1_0.EstateResource;
+import com.liferay.sampleVue.dto.v1_0.Estate;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ServiceScope;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.tags.Tags;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Parameter;
 
 /**
  * @author David Brenes
@@ -13,4 +29,43 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = EstateResource.class
 )
 public class EstateResourceImpl extends BaseEstateResourceImpl {
+
+		/**
+	 * Invoke this method with the command line:
+	 *
+	 * curl -X 'GET' 'http://localhost:8080/o/sample-vue/v1.0/estates/{municipalityId}/{postalCode}/{addressKind}/{addressName}'  -u 'test@liferay.com:test'
+	 */
+	@Override
+	@GET
+	@Operation(
+		description = "Get all the estates for a whole address from Anidia CRM"
+	)
+	@Parameters(
+		value = {
+			@Parameter(in = ParameterIn.PATH, name = "municipalityId"),
+			@Parameter(in = ParameterIn.PATH, name = "postalCode"),
+			@Parameter(in = ParameterIn.PATH, name = "addressKind"),
+			@Parameter(in = ParameterIn.PATH, name = "addressName")
+		}
+	)
+	@Path("/estates/{municipalityId}/{postalCode}/{addressKind}/{addressName}")
+	@Produces({"application/json", "application/xml"})
+	@Tags(value = {@Tag(name = "Estate")})
+	public Page<Estate> getEstatesAddressNamePage(
+			@NotNull @Parameter(hidden = true) @PathParam("municipalityId")
+				String municipalityId,
+			@NotNull @Parameter(hidden = true) @PathParam("postalCode") String
+				postalCode,
+			@NotNull @Parameter(hidden = true) @PathParam("addressKind") String
+				addressKind,
+			@NotNull @Parameter(hidden = true) @PathParam("addressName") String
+				addressName)
+		throws Exception {
+
+			SalesforceService salesforce = new SalesforceService();
+
+		return Page.of(salesforce.getEstates(municipalityId, postalCode, addressKind, addressName));
+	}
+
+
 }
