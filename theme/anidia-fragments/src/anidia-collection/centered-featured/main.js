e0d7631ddd28846b192caabf6e5b09ea079667e8
Clara Morrondo <clara.morrondo@the-cocktail.com>
2020-10-23 10:35:35 +0200

    Hero and centered feaure fixes

--- theme/anidia-fragments/src/anidia-collection/centered-featured/main.js
+++ theme/anidia-fragments/src/anidia-collection/centered-featured/main.js
@@ -1,3 +1,10 @@
-console.group('centered-featured');
-console.log('fragmentElement', fragmentElement);
-console.groupEnd();
+function centeredFeatured() {
+  const container = document.querySelector(".an-centered-featured");
+  const src = document.querySelector("#ImagenFondo img").src;
+  container.style.backgroundImage = 'url(' + src + ')';
+}
+
+
+if (document.querySelector(".an-centered-featured")) {
+  centeredFeatured();
+}
