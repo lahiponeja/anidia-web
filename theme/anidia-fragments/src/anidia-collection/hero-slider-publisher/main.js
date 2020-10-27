5ab4b30ed298e05e6bfba223ee6346579f2f581e
Clara Morrondo <clara.morrondo@the-cocktail.com>
2020-10-27 12:58:59 +0100

    minor fixes

--- theme/anidia-fragments/src/anidia-collection/hero-slider-publisher/main.js
+++ theme/anidia-fragments/src/anidia-collection/hero-slider-publisher/main.js
@@ -71,10 +71,9 @@ function heroSlider() {
     xPercent: -100 * (sections.length - 1),
     ease: "none",
     scrollTrigger: {
-      trigger: ".an-hero-slider__inner-wrapper",
+      trigger: ".an-hero-slider",
       pin: true,
       scrub: 1,
-      snap: 1 / (sections.length - 1),
       // base vertical scrolling on how wide the container is so it feels more natural.
       end: () =>  "+=" + (document.querySelector(".an-hero-slider__item").offsetWidth * sections.length),
       onUpdate: debounce(checkLeftCollision, 500)
