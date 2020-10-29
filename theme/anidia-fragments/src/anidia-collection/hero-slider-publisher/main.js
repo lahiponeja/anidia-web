1062c86a8e81144176adc0e3320af4024b027908
Clara Morrondo <clara.morrondo@the-cocktail.com>
2020-10-29 16:14:18 +0100

    scrollTo added when discoverBtn clicked

--- theme/anidia-fragments/src/anidia-collection/hero-slider-publisher/main.js
+++ theme/anidia-fragments/src/anidia-collection/hero-slider-publisher/main.js
@@ -88,4 +88,16 @@ function heroSlider() {
 if (document.querySelectorAll(".an-hero-slider__item").length > 1) {
   heroSlider();
   document.getElementById('banner').classList.add('more-elem');
+} else {
+  const discoverBtn = document.querySelectorAll("[data-discover-btn]");
+  discoverBtn.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      window.scrollTo({
+        top: document.querySelector(".an-hero-slider__item").offsetWidth - 300,
+        behavior: 'smooth',
+      });
+    });
+  });
 }
+
