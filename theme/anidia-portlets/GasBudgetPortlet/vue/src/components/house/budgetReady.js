4a63e9c5da9577ae9fb73d98a973e5a94181e2c4
Diego Palacios <diego.palacios@the-cocktail.com>
2020-11-19 18:46:48 +0100

    Added flag in flow inputs.

--- theme/anidia-portlets/GasBudgetPortlet/vue/src/components/house/budgetReady.js
+++ theme/anidia-portlets/GasBudgetPortlet/vue/src/components/house/budgetReady.js
@@ -60,7 +60,10 @@ const budgetReady = {
           <input v-model="budgetReadyForm.lastname" type="text" class="an-input__field" placeholder="Apellidos*" required="">
         </div>
         <div class="an-input an-form__item">
-          <div class="an-select an-select--small-width mr-xs">
+          <div class="an-select an-select--flag an-select--small-width mr-xs">
+            <template v-for="(option, index) in phonePrefixesOptions">
+              <div class="an-select__flag" v-if="option.value === phonePrefix" v-html="option.flagImg"></div>
+            </template>
             <span class="an-select__icon an-icon--chevron-down"></span>
             <select v-model="phonePrefix" class="an-select__native" required>
               <option v-for="(option, index) in phonePrefixesOptions" :value="option.value">
