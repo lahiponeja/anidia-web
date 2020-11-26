96da0cd2087b427f29e2ecde145d20c2d49e7c9c
Diego Palacios <diego.palacios@the-cocktail.com>
2020-11-26 01:57:14 +0100

    Add opacity, top and bottom animations to the slider.

--- theme/anidia-fragments/src/anidia-collection/hero-slider-publisher/main.js
+++ theme/anidia-fragments/src/anidia-collection/hero-slider-publisher/main.js
@@ -1,21 +1,7 @@
 function heroSlider() {
-  const dataSliderLinesList = document.querySelector("[data-slider-lines-list]");
   var slides = document.querySelectorAll(".an-hero-slider .journal-content-article");
 
-  slides.forEach((el, i) => {
-    if (i === 0) el.classList.add('first');
-    else {
-      if (i === slides.length - 1) el.classList.add('last');
-      else el.classList.add('inner');
-    }
-  });
-
-  const linesHTML = []
-  for(let i = 0; i < slides.length; i++) {
-    linesHTML.push(`<div class="an-hero-slider__lines-item an-hero-slider__lines-item-${(i + 1)} ${ i === 0 && "an-hero-slider__lines-item--active"}" data-slide-line></div>`);
-  }
-  dataSliderLinesList.innerHTML = linesHTML.join("");
-
+  setSliderLines(slides);
 
   function getSiblings(elem) {
     var siblings = [];
@@ -25,43 +11,51 @@ function heroSlider() {
       if ( sibling.nodeType == 1 && sibling != elem )
           siblings.push( sibling );
     return siblings;
-}
+  }
 
-function checkLeftCollision() {
-  const slideLine = document.querySelectorAll("[data-slide-line]");
+  function checkLeftCollision() {
+    const slideLine = document.querySelectorAll("[data-slide-line]");
 
-  slides.forEach((slide, i) => {
-    const slideRect = slide.getBoundingClientRect();
-    if(slideLine.length) {
-      if(Math.abs(slideRect.top).toFixed(0) < 300) {
-      const slideLineSiblings = getSiblings(slideLine[i]);
+    slides.forEach((slide, i) => {
+      const info = slide.querySelector('.an-hero-slider__info')
+      const opacityNum = window.getComputedStyle(info).getPropertyValue("opacity")
+      const slideRect = slide.getBoundingClientRect();
+      if(slideLine.length) {
+        // if(Math.abs(slideRect.top).toFixed(0) < 300) {
+        if(opacityNum > 0) {
+        const slideLineSiblings = getSiblings(slideLine[i]);
 
-        slideLine[i].classList.add('an-hero-slider__lines-item--active');
+          slideLine[i].classList.add('an-hero-slider__lines-item--active');
 
-        slideLineSiblings.forEach((siblingLine) => {
-          siblingLine.classList.remove('an-hero-slider__lines-item--active');
-        });
+          slideLineSiblings.forEach((siblingLine) => {
+            siblingLine.classList.remove('an-hero-slider__lines-item--active');
+          });
+        }
       }
-    }
-  });
-}
+    });
+  }
 
   gsap.registerPlugin(ScrollTrigger);
 
   var tl = new TimelineMax();
-  tl
-  .to(".first .an-hero-slider__image", {y: 0, duration: 20, display: 'none'})
-  // DIEGO .an-hero-slider__info
-  .to(".first .an-hero-slider__info", {y: 0, duration: 0, display: 'none'})
+  tl.to(".first.journal-content-article .an-hero-slider__image", { duration: 30,  opacity: 0})
+  tl.to(".first.journal-content-article .an-hero-slider__info", { duration: 27,  opacity: 0, y:-100}, "-=30")
 
-  .to(".inner .an-hero-slider__image", {y: 0, duration: 10, display: 'block'})
-  .to(".inner .an-hero-slider__info", {y: 0, duration: 20, display: 'block'})
+  if(document.querySelectorAll(".inner.journal-content-article").length > 0) {
+    Array.from(document.querySelectorAll(".inner.journal-content-article")).forEach((elem, index) => {
+      const img = elem.querySelector(".an-hero-slider__image")
+      const info = elem.querySelector(".an-hero-slider__info")
 
-  .to(".inner .an-hero-slider__image", {y: 0, duration: 10, display: 'none'})
-  .to(".inner .an-hero-slider__info", {y: 0, duration: 0, display: 'none'})
+      tl.to(img, {duration: 30,  opacity: 1})
+      tl.to(info, {duration: 27, opacity: 1, y: 0}, "-=30")
 
-  .to(".last .an-hero-slider__image", {y: 0, duration: 20, display: 'block'})
-  .to(".last .an-hero-slider__info", {y: 0, duration: 20, display: 'block'});
+      tl.to(img, {duration: 30,  opacity: 0})
+      tl.to(info, {duration: 27, opacity: 0, y:-100}, "-=30")
+    });
+  }
+
+  tl.to(".last.journal-content-article .an-hero-slider__image", {duration: 30,  opacity: 1})
+  tl.to(".last.journal-content-article .an-hero-slider__info", {duration: 27, opacity: 1, y:0}, "-=30")
 
   const controller = new ScrollMagic.Controller();
 
@@ -79,24 +73,44 @@ function checkLeftCollision() {
   })
   .on('leave', endBgAnimate);
 
-
   function endBgAnimate () {
     document.getElementById('banner').style.opacity = '0';
     document.querySelector('.an-hero-slider__lines').style.opacity = '0';
   }
 
   function startBgAnimate () {
-    document.getElementById('banner').style.opacity = '1';
-    document.querySelector('.an-hero-slider__lines').style.opacity = '1';
+    if(document.getElementById('banner') && document.querySelector('.an-hero-slider__lines')) {
+      document.getElementById('banner').style.opacity = '1';
+      document.querySelector('.an-hero-slider__lines').style.opacity = '1';
+    }
   }
 
 }
 
-if (document.querySelectorAll(".an-hero-slider__item").length > 1) {
-  heroSlider();
-  document.getElementById('banner').classList.add('more-elem');
+function setSliderLines(slides) {
+  const dataSliderLinesList = document.querySelector("[data-slider-lines-list]");
+
+  slides.forEach((el, i) => {
+    if (i === 0) el.classList.add('first');
+    else {
+      if (i === slides.length - 1) el.classList.add('last');
+      else el.classList.add('inner');
+    }
+  });
+
+  const linesHTML = []
+  for(let i = 0; i < slides.length; i++) {
+    linesHTML.push(`<div class="an-hero-slider__lines-item an-hero-slider__lines-item-${(i + 1)} ${ i === 0 && "an-hero-slider__lines-item--active"}" data-slide-line></div>`);
+  }
+  dataSliderLinesList.innerHTML = linesHTML.join("");
 }
 
+if (document.querySelectorAll(".an-hero-slider__item").length > 1) {
+  heroSlider();
+  if(document.getElementById('banner')) {
+    document.getElementById('banner').classList.add('more-elem');
+  }
+}
 
 const discoverBtn = document.querySelectorAll("[data-discover-btn]");
 
@@ -111,4 +125,3 @@ if (discoverBtn) {
     });
   });
 }
-
