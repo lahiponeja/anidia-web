ef207008b9fc80656a7ff22424e2690ee0e50e26
David J. Brenes <david.brenes@the-cocktail.com>
2021-02-23 13:10:22 +0100

    Included clases to handle the LEad request in solar portlet

---
+++ theme/anidia-portlets/SolarBudgetPortlet/impl/src/main/java/com/liferay/solarBudget/internal/dto/PersonalDataRequest.java
@@ -0,0 +1,186 @@
+
+package com.liferay.solarBudget.internal.dto;
+
+public class PersonalDataRequest {
+
+    //@JsonProperty("AcceptNotCom")
+    private Boolean acceptNotCom;
+    //@JsonProperty("AddressBloq")
+    private String addressBloq;
+    //@JsonProperty("AddressDoor")
+    private String addressDoor;
+    //@JsonProperty("AddressNumber")
+    private String addressNumber;
+    //@JsonProperty("AddressPlant")
+    private String addressPlant;
+    //@JsonProperty("AddressPostalCode")
+    private String addressPostalCode;
+    //@JsonProperty("AddressStair")
+    private String addressStair;
+    //@JsonProperty("AddressStreet")
+    private String addressStreet;
+    //@JsonProperty("AddressTown")
+    private String addressTown;
+    //@JsonProperty("Email")
+    private String email;
+    //@JsonProperty("FirstName")
+    private String firstName;
+    //@JsonProperty("LastName")
+    private String lastName;
+    //@JsonProperty("Phone")
+    private String phone;
+    //@JsonProperty("ProdInterest")
+    private String prodInterest;
+    //@JsonProperty("PrivacyPolicy")
+    private String privacyPolicy;
+
+    private String codMunicipio;
+
+    private String codFinca;
+
+    private String codInmueble;
+
+    public Boolean getAcceptNotCom() {
+        return acceptNotCom;
+    }
+
+    public void setAcceptNotCom(Boolean acceptNotCom) {
+        this.acceptNotCom = acceptNotCom;
+    }
+
+    public String getAddressBloq() {
+        return addressBloq;
+    }
+
+    public void setAddressBloq(String addressBloq) {
+        this.addressBloq = addressBloq;
+    }
+
+    public String getAddressDoor() {
+        return addressDoor;
+    }
+
+    public void setAddressDoor(String addressDoor) {
+        this.addressDoor = addressDoor;
+    }
+
+    public String getAddressNumber() {
+        return addressNumber;
+    }
+
+    public void setAddressNumber(String addressNumber) {
+        this.addressNumber = addressNumber;
+    }
+
+    public String getAddressPlant() {
+        return addressPlant;
+    }
+
+    public void setAddressPlant(String addressPlant) {
+        this.addressPlant = addressPlant;
+    }
+
+    public String getAddressPostalCode() {
+        return addressPostalCode;
+    }
+
+    public void setAddressPostalCode(String addressPostalCode) {
+        this.addressPostalCode = addressPostalCode;
+    }
+
+    public String getAddressStair() {
+        return addressStair;
+    }
+
+    public void setAddressStair(String addressStair) {
+        this.addressStair = addressStair;
+    }
+
+    public String getAddressStreet() {
+        return addressStreet;
+    }
+
+    public void setAddressStreet(String addressStreet) {
+        this.addressStreet = addressStreet;
+    }
+
+    public String getAddressTown() {
+        return addressTown;
+    }
+
+    public void setAddressTown(String addressTown) {
+        this.addressTown = addressTown;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getProdInterest() {
+        return prodInterest;
+    }
+
+    public void setProdInterest(String prodInterest) {
+        this.prodInterest = prodInterest;
+    }
+
+    public String getCodMunicipio() {
+        return codMunicipio;
+    }
+
+    public void setCodMunicipio(String codMunicipio) {
+        this.codMunicipio = codMunicipio;
+    }
+
+    public String getCodFinca() {
+        return codFinca;
+    }
+
+    public void setCodFinca(String codFinca) {
+        this.codFinca = codFinca;
+    }
+
+    public String getCodInmueble() {
+        return codInmueble;
+    }
+
+    public void setCodInmueble(String codInmueble) {
+        this.codInmueble = codInmueble;
+    }
+
+    public String getPrivacyPolicy() {
+        return privacyPolicy;
+    }
+
+    public void setPrivacyPolicy(String privacyPolicy) {
+        this.privacyPolicy = privacyPolicy;
+    }
+}
---
+++ theme/anidia-portlets/SolarBudgetPortlet/impl/src/main/java/com/liferay/solarBudget/internal/services/SalesforceService.java
@@ -0,0 +1,317 @@
+package com.liferay.solarBudget.internal.services;
+
+import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.databind.*;
+import com.liferay.solarBudget.dto.v1_0.*;
+import com.liferay.solarBudget.internal.dto.*;
+import com.liferay.solarBudget.internal.exception.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.net.*;
+import java.net.http.*;
+import java.util.*;
+import org.json.*;
+
+
+public class SalesforceService {
+
+	static String SALESFORCE_TOKEN_URL = System.getenv().get("SALESFORCE_TOKEN_URL");
+	static String SALESFORCE_LEAD_URL = System.getenv().get("SALESFORCE_SOLAR_LEAD_URL");
+	static String SALESFORCE_PASSWORD = System.getenv().get("SALESFORCE_PASSWORD");
+	static String SALESFORCE_CLIENT_SECRET = System.getenv().get("SALESFORCE_CLIENT_SECRET");
+	static String SALESFORCE_CLIENT_ID = System.getenv().get("SALESFORCE_CLIENT_ID");
+	static String SALESFORCE_USERNAME = System.getenv().get("SALESFORCE_USERNAME");
+
+	private String getSalesforceToken() {
+
+		StringBuilder urlBuilder = new StringBuilder();
+		urlBuilder.append(SALESFORCE_TOKEN_URL);
+		urlBuilder.append("?");
+		urlBuilder.append("grant_type=password");
+		urlBuilder.append("&client_id=");
+		urlBuilder.append(SALESFORCE_CLIENT_ID);
+		urlBuilder.append("&client_secret=");
+		urlBuilder.append(SALESFORCE_CLIENT_SECRET);
+		urlBuilder.append("&username=");
+		urlBuilder.append(SALESFORCE_USERNAME);
+		urlBuilder.append("&password=");
+		urlBuilder.append(SALESFORCE_PASSWORD);
+
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder().
+			uri(URI.create(urlBuilder.toString())).
+			POST(HttpRequest.BodyPublishers.noBody()).
+			build();
+
+		HttpResponse<String> response = null;
+		try {
+			response = client.send(request, HttpResponse.BodyHandlers.ofString());
+		} catch (IOException | InterruptedException e) {
+			if(response != null) {
+				System.out.println(response.body());
+			}
+			e.printStackTrace();
+			return null;
+		}
+
+		JSONObject json;
+		try {
+			json = new JSONObject(response.body());
+		} catch (JSONException e) {
+			System.out.println(response.body());
+			e.printStackTrace();
+			return null;
+		}
+
+		try {
+			return json.getString("access_token");
+		} catch (JSONException e) {
+			System.out.println(json.toString());
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public Lead createLead(Lead lead) {
+		String token = this.getSalesforceToken();
+
+		StringBuilder urlBuilder = new StringBuilder();
+		urlBuilder.append(SALESFORCE_LEAD_URL);
+
+		System.out.println("URL Crear lead: " + urlBuilder.toString());
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder().
+			uri(URI.create(urlBuilder.toString())).
+			header("Authorization", "Bearer " + token).
+			POST(HttpRequest.BodyPublishers.ofString(mapRequestBody(lead))).
+			build();
+
+		HttpResponse<String> response = null;
+		try {
+			response = client.send(request, HttpResponse.BodyHandlers.ofString());
+			if (response.statusCode() != 200 && response.statusCode() != 201) {
+				throw new PortletException(2, "Error creating lead");
+			}
+		} catch (IOException | InterruptedException | PortletException e) {
+			if(response != null) {
+				System.out.println(response.body());
+			}
+			e.printStackTrace();
+			return null;
+		}
+
+		System.out.println("** Lead creado correctamente ** " + response.statusCode());
+		System.out.println("** Respuesta creado lead ** " + response.body());
+		return lead;
+	}
+
+	private String mapRequestBody(Lead lead){
+
+		ObjectMapper mapper = new ObjectMapper();
+		String mapperString = "";
+		try {
+			mapperString = mapper.writeValueAsString(mapToSendLeadRequest(lead));
+			System.out.println("** String lead request: " + mapperString);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		return mapperString;
+	}
+
+	/**
+	 *
+	 * @param lead
+	 * @return
+	 */
+	private SendLeadRequest mapToSendLeadRequest(Lead lead) {
+
+		SendLeadRequest sendLeadRequest = new SendLeadRequest();
+
+		sendLeadRequest.setPersonalData(mapToPersonalDataRequest(lead.getPersonalData()));
+    sendLeadRequest.setCalculatorSolar(mapToCalculatorSolar(lead.getCalculatorSolar()));
+    sendLeadRequest.getPersonalData().setPrivacyPolicy("GasServicios");
+
+		return sendLeadRequest;
+	}
+
+	/**
+	 *
+	 * @param calculatorSolar
+	 * @return
+	 */
+	private CalculatorSolarRequest mapToCalculatorSolar(CalculatorSolar calculatorSolar) {
+		CalculatorSolarRequest calculatorSolarRequest = new CalculatorSolarRequest();
+		calculatorSolarRequest.setSuperiorInstallation(calculatorSolar.getSuperiorInstallation());
+		calculatorSolarRequest.setFinalPrice(calculatorSolar.getFinalPrice());
+		calculatorSolarRequest.setInstallerCode(calculatorSolar.getInstallerCode());
+
+		calculatorSolarRequest.setSelectedExtras(maptoSelectedExtras(calculatorSolar.getSelectedExtras()));
+		calculatorSolarRequest.setInput(mapToSolarInputRequest(calculatorSolar.getInput()));
+		calculatorSolarRequest.setOutput(mapToSolarOutputRequest(calculatorSolar.getOutput()));
+
+		return calculatorSolarRequest;
+	}
+
+		/**
+	 *
+	 * @param selectedExtras
+	 * @return
+	 */
+	private SelectedSolarExtrasRequest maptoSelectedExtras(SelectedExtras selectedExtras) {
+		SelectedSolarExtrasRequest extras = new SelectedSolarExtrasRequest();
+		extras.setExtraPanels(selectedExtras.getExtraPanels());
+		extras.setTriphasicExtra(selectedExtras.getTriphasicExtra());
+		extras.setRoofExtra(selectedExtras.getRoofExtra());
+		extras.setPergolaExtra(selectedExtras.getPergolaExtra());
+		extras.setPipelineUnderground(selectedExtras.getPipelineUnderground());
+		extras.setBattery(selectedExtras.getBattery());
+		extras.setCarCharger(selectedExtras.getCarCharger());
+		return extras;
+	}
+
+	/**
+	 *
+	 * @param calculatorSolarInput
+	 * @return
+	 */
+	private SolarInputRequest mapToSolarInputRequest(SolarBudgetRequest calculatorSolarInput) {
+		SolarInputRequest inputRequest = new SolarInputRequest();
+		inputRequest.setHouseType(calculatorSolarInput.getHouseTypeAsString());
+		inputRequest.setMonthlyConsumption(calculatorSolarInput.getMonthlyConsumption());
+		inputRequest.setRoofType(calculatorSolarInput.getRoofTypeAsString());
+		return inputRequest;
+	}
+
+	/**
+	 *
+	 * @param calculatorSolarOutput
+	 * @return
+	 */
+	private SolarOutputRequest mapToSolarOutputRequest(SolarBudget calculatorSolarOutput) {
+		SolarOutputRequest outputRequest = new SolarOutputRequest();
+		outputRequest.setPanelsType(calculatorSolarOutput.getPanelsType());
+		outputRequest.setPanelsExtra(calculatorSolarOutput.getPanelsExtra());
+		outputRequest.setTriphasicExtra(calculatorSolarOutput.getTriphasicExtra());
+		outputRequest.setInverterExtra(calculatorSolarOutput.getInverterExtra());
+		outputRequest.setRoofExtra(calculatorSolarOutput.getRoofExtra());
+		outputRequest.setPergolaExtra(calculatorSolarOutput.getPergolaExtra());
+		outputRequest.setPipelineExtra(calculatorSolarOutput.getPipelineExtra());
+		outputRequest.setCarCharger(calculatorSolarOutput.getCarCharger());
+		outputRequest.setBattery(calculatorSolarOutput.getBattery());
+		outputRequest.setAdditionalPanelsInstallation(calculatorSolarOutput.getAdditionalPanelsInstallation());
+		outputRequest.setTotalPrice(calculatorSolarOutput.getTotalPrice());
+		outputRequest.setSize(mapToSolarSize(calculatorSolarOutput.getSize()));
+		outputRequest.setInverter(mapToSolarInverter(calculatorSolarOutput.getInverter()));
+		outputRequest.setSuperiorInstallation(mapToSolarSuperiorInstallation(calculatorSolarOutput.getSuperiorInstallation()));
+
+		return outputRequest;
+	}
+	/**
+	 *
+	 * @param budgetSize
+	 * @return
+	 */
+	private SolarSize mapToSolarSize(SolarBudgetSize budgetSize) {
+		SolarSize size = new SolarSize();
+		size.setValue(budgetSize.getValue());
+		size.setUnitPrice(budgetSize.getUnitPrice());
+		size.setPrice(budgetSize.getPrice());
+		size.setBasePanels(budgetSize.getBasePanels());
+		size.setTotalPanels(budgetSize.getTotalPanels());
+		return size;
+	}
+
+	/**
+	 *
+	 * @param solarInverter
+	 * @return
+	 */
+	private SolarInverter mapToSolarInverter(SolarOutputInverter solarInverter) {
+		SolarInverter inverter = new SolarInverter();
+		inverter.setBrand(solarInverter.getBrand());
+		inverter.setModel(solarInverter.getModel());
+		inverter.setPrice(solarInverter.getPrice());
+
+		return inverter;
+	}
+
+	/**
+	 *
+	 * @param superiorInstallation
+	 * @return
+	 */
+	private SolarSuperiorInstallation mapToSolarSuperiorInstallation(SuperiorInstallation superiorInstallation) {
+		SolarSuperiorInstallation sInstallation = new SolarSuperiorInstallation();
+		sInstallation.setPanelsType(superiorInstallation.getPanelsType());
+		sInstallation.setPanelsExtra(superiorInstallation.getPanelsExtra());
+		sInstallation.setTriphasicExtra(superiorInstallation.getTriphasicExtra());
+		sInstallation.setInverterExtra(superiorInstallation.getInverterExtra());
+		sInstallation.setInverterType(superiorInstallation.getInverterType());
+		sInstallation.setRoofExtra(superiorInstallation.getRoofExtra());
+		sInstallation.setPergolaExtra(superiorInstallation.getPergolaExtra());
+		sInstallation.setPipelineExtra(superiorInstallation.getPipelineExtra());
+		sInstallation.setCarCharger(superiorInstallation.getCarCharger());
+		sInstallation.setBattery(superiorInstallation.getBattery());
+		sInstallation.setExtraFornius(superiorInstallation.getExtraFornius());
+		sInstallation.setAdditionalPanelsInstallation(superiorInstallation.getAdditionalPanelsInstallation());
+		sInstallation.setSuperiorSize(mapToSolarSuperiorSize(superiorInstallation.getSuperiorSize()));
+
+		return sInstallation;
+	}
+
+		/**
+	 *
+	 * @param superiorSize
+	 * @return
+	 */
+	private SolarSuperiorSize mapToSolarSuperiorSize(SuperiorSize superiorSize) {
+		SolarSuperiorSize size = new SolarSuperiorSize();
+		size.setValue(superiorSize.getValue());
+		size.setPrice(superiorSize.getPrice());
+		size.setBasePanels(superiorSize.getBasePanels());
+		return size;
+	}
+
+
+	/**
+	 *
+	 * @param personalData
+	 * @return
+	 */
+	private PersonalDataRequest mapToPersonalDataRequest(PersonalData personalData) {
+		PersonalDataRequest personalDataRequest = new PersonalDataRequest();
+
+		personalDataRequest.setAcceptNotCom(personalData.getAcceptNotCom());
+		personalDataRequest.setEmail(personalData.getEmail());
+		personalDataRequest.setFirstName(personalData.getFirstName());
+		personalDataRequest.setLastName(personalData.getLastName());
+		personalDataRequest.setPhone(personalData.getPhone());
+
+		if (personalData.getProperty() != null) {
+			personalDataRequest.setAddressBloq(personalData.getProperty().getBlock());
+			personalDataRequest.setAddressDoor(personalData.getProperty().getDoor());
+			personalDataRequest.setAddressPlant(personalData.getProperty().getFloor());
+			personalDataRequest.setAddressStair(personalData.getProperty().getLadder());
+			personalDataRequest.setCodInmueble(personalData.getProperty().getPropertyId());
+		}
+
+		if (personalData.getEstate() != null) {
+			personalDataRequest.setAddressNumber(personalData.getEstate().getNumber());
+			personalDataRequest.setAddressStreet(personalData.getEstate().getAddressName());
+			personalDataRequest.setCodFinca(personalData.getEstate().getGateId());
+		}
+
+		if (personalData.getPostalCode() != null) {
+			personalDataRequest.setAddressPostalCode(personalData.getPostalCode().getPostalCode());
+			personalDataRequest.setAddressTown(personalData.getPostalCode().getMunicipalityName());
+			personalDataRequest.setCodMunicipio(personalData.getPostalCode().getProvinceId() + personalData.getPostalCode().getMunicipalityId());
+		}
+
+		if (personalData.getProdInterest() != null) {
+			personalDataRequest.setProdInterest(personalData.getProdInterest().getValue());
+		}
+
+		return personalDataRequest;
+	}
+
+}
