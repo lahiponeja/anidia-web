0314e50a2554ee76c62dc5d109ead5365ff18a42
David J. Brenes <david.brenes@the-cocktail.com>
2021-02-25 09:17:37 +0100

    Replaced validation text for phone

--- theme/anidia-portlets/GasBudgetPortlet/vue/src/components/business/businessContactForm.js
+++ theme/anidia-portlets/GasBudgetPortlet/vue/src/components/business/businessContactForm.js
@@ -100,7 +100,7 @@ const businessContactForm = {
             </div>
           </div>
 
-          <input v-model="phoneNumber" v-on:input="reportPhoneValidation" v-on:invalid="setPhoneCustomValidationMessage" data-validation-message="Introduce un número de teléfono, sin espacios ni guiones." type="text" pattern="\\d{9}" class="an-input__field" placeholder="Teléfono" required="">
+          <input v-model="phoneNumber" v-on:input="reportPhoneValidation" v-on:invalid="setPhoneCustomValidationMessage" data-validation-message="Introduce tu número completo, sin espacios ni guiones." type="text" pattern="\\d{9}" class="an-input__field" placeholder="Teléfono" required="">
         </div>
         <div class="an-input an-form__item">
           <input v-model="businessFormData.email" type="email" class="an-input__field" placeholder="Email*" required="">
