afb085204eba8a28bed5349d843eb14cea210cd2
David J. Brenes <david.brenes@the-cocktail.com>
2021-02-25 16:55:57 +0100

    Fixing telephone field

--- theme/anidia-portlets/SolarBudgetPortlet/vue/src/components/house/budgetReady.js
+++ theme/anidia-portlets/SolarBudgetPortlet/vue/src/components/house/budgetReady.js
@@ -114,7 +114,7 @@ const budgetReady = {
               </div>
             </div>
           </div>
-          <input v-model="phoneNumber" v-on:input="reportPhoneValidation" v-on:invalid="setPhoneCustomValidationMessage" data-validation-message="Introduce un número de teléfono, sin espacios ni guiones." type=
+          <input v-model="phoneNumber" v-on:input="reportPhoneValidation" v-on:invalid="setPhoneCustomValidationMessage" data-validation-message="Introduce tu número completo, sin espacios ni guiones." type="text" pattern="\\d{9}" class="an-input__field" placeholder="Teléfono*" required="">
         </div>
         <div class="an-input an-form__item">
           <input v-model="budgetReadyForm.email" type="email" class="an-input__field" placeholder="Email*" required="">
