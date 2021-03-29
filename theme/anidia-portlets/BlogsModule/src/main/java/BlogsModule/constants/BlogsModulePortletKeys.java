eb3bf157bd268ca6b2fbda8dc2d90c10dfc43c2c
Francisco Serrano <francisco.serrano@the-cocktail.com>
2021-03-29 15:06:10 +0200

    BlogsModule Portlet first commit

---
+++ theme/anidia-portlets/BlogsModule/src/main/java/BlogsModule/constants/BlogsModulePortletKeys.java
@@ -0,0 +1,11 @@
+package BlogsModule.constants;
+
+/**
+ * @author danieldelapena
+ */
+public class BlogsModulePortletKeys {
+
+	public static final String BLOGSMODULE =
+		"BlogsModule_BlogsModulePortlet";
+
+}
