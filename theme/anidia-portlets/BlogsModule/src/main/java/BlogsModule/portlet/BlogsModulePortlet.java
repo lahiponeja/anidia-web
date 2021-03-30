32e164bf52f8499e28b3cf70950061d732874441
Francisco Serrano <francisco.serrano@the-cocktail.com>
2021-03-30 11:23:48 +0200

    it retrieves the blogs entries with search term, tag or without any filter

--- theme/anidia-portlets/BlogsModule/src/main/java/BlogsModule/portlet/BlogsModulePortlet.java
+++ theme/anidia-portlets/BlogsModule/src/main/java/BlogsModule/portlet/BlogsModulePortlet.java
@@ -1,50 +1,32 @@
 package BlogsModule.portlet;
 
 import BlogsModule.constants.BlogsModulePortletKeys;
-
 import com.liferay.asset.kernel.model.AssetTag;
 import com.liferay.asset.kernel.service.AssetEntryLocalServiceUtil;
 import com.liferay.asset.kernel.service.AssetTagLocalServiceUtil;
 import com.liferay.asset.kernel.service.persistence.AssetEntryQuery;
-import com.liferay.blogs.service.*;
-import com.liferay.blogs.model.*;
-import com.liferay.blogs.exception.*;
-import com.liferay.blogs.configuration.*;
-import com.liferay.blogs.constants.*;
-import com.liferay.portal.kernel.dao.orm.DynamicQuery;
-import com.liferay.portal.kernel.dao.orm.DynamicQueryFactoryUtil;
+import com.liferay.blogs.model.BlogsEntry;
+import com.liferay.blogs.service.BlogsEntryLocalServiceUtil;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
-
-import javax.portlet.*;
-
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.search.document.Document;
+import com.liferay.portal.search.query.BooleanQuery;
+import com.liferay.portal.search.query.FuzzyQuery;
+import com.liferay.portal.search.query.Queries;
+import com.liferay.portal.search.searcher.*;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.WebKeys;
-
+import javax.portlet.*;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import com.liferay.asset.kernel.model.AssetCategory;
-import com.liferay.asset.kernel.model.AssetEntry;
-import com.liferay.asset.kernel.service.AssetCategoryLocalServiceUtil;
-import com.liferay.journal.model.JournalArticle;
-import com.liferay.journal.service.JournalArticleLocalServiceUtil;
-import com.liferay.portal.kernel.json.JSONException;
-
-import com.liferay.portal.kernel.xml.*;
-
-import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.asset.list.asset.entry.provider.AssetListAssetEntryProvider;
-import com.liferay.asset.list.model.AssetListEntry;
-import com.liferay.asset.list.service.AssetListEntryLocalServiceUtil;
 
 
 /*    
@@ -67,140 +49,124 @@ import com.liferay.asset.list.service.AssetListEntryLocalServiceUtil;
 )
 
 public class BlogsModulePortlet extends MVCPortlet {
-	
-	public static final String COLLECTION_NAME = "repertorio-faqs";
+
 	@Reference
-	private AssetListAssetEntryProvider _assetListAssetEntryProvider;
+	protected Queries queries;
 
-	@ProcessAction(name = "prueba")
-	public void prueba(ActionRequest request, ActionResponse response) {
-		request.setAttribute("mensaje","HOla "+ParamUtil.getString(request, "nombreEscritor"));
-	}
+	@Reference
+	protected Searcher searcher;
 
-	@ProcessAction(name = "prueba2")
-	public void prueba2(ActionRequest request, ActionResponse response) {
-		request.setAttribute("mensaje2","HOla pepillllo");
-	}
+	@Reference
+	protected SearchRequestBuilderFactory searchRequestBuilderFactory;
 
 	@Override
 	public void doView(RenderRequest renderRequest, RenderResponse renderResponse) throws IOException, PortletException {
 		ThemeDisplay themeDisplay = (ThemeDisplay)renderRequest.getAttribute(WebKeys.THEME_DISPLAY);
+		List<BlogsEntry> blogEntries;
+		int pageSize = 7;
 
 		String searchTerm = ParamUtil.getString(renderRequest, "searchTerm");
+		String searchTag = ParamUtil.getString(renderRequest,"searchTag");
+		int page = ParamUtil.getInteger(renderRequest,"page");
 
-		int entriesCount = BlogsEntryLocalServiceUtil.getBlogsEntriesCount();
-		List<BlogsEntry> blogEntries = BlogsEntryLocalServiceUtil.getBlogsEntries(0,entriesCount);
-		//List<BlogsEntry> blogEntries = getBlogsByTag(themeDisplay,"etiqueta-primavera",0,entriesCount)
+		if ( searchTag!= null && !searchTag.equals("")) {
+			blogEntries = getBlogsByTag(themeDisplay,searchTag,(page - 1) * pageSize,pageSize * page);
+		} else if (searchTerm!= null && !searchTerm.equals("")) {
+			blogEntries = getBlogsBySearch(themeDisplay, searchTerm);
+		} else {
+			blogEntries = BlogsEntryLocalServiceUtil.getBlogsEntries((page - 1) * pageSize,pageSize * page);
+		}
 
 		JSONObject jsonReturn = new JSONObject();
+		jsonReturn.append("data", new JSONObject());
+
 		blogEntries.forEach(entry -> {
-			JSONObject jsonEntry = new JSONObject();
-			jsonEntry.put("title",entry.getTitle());
-			jsonEntry.put("subtitle",entry.getSubtitle());
-			jsonEntry.put("date",entry.getDisplayDate());
-			jsonEntry.put("content",entry.getContent());
+			if(!entry.isExpired() && !entry.isInTrash() && !entry.isDraft()) {
+				JSONObject jsonEntry = new JSONObject();
+				jsonEntry.put("title",entry.getTitle());
+				jsonEntry.put("subtitle",entry.getSubtitle());
+				jsonEntry.put("date",entry.getDisplayDate());
+				jsonEntry.put("content",entry.getContent());
+				jsonEntry.put("url",entry.getUrlTitle());
 
-			JSONArray tags = new JSONArray();
-			AssetTagLocalServiceUtil.getTags(BlogsEntry.class.getName(),entry.getEntryId()).forEach(tag -> {
-				tags.put(tag.getName());
-			});
-			jsonEntry.put("tags",tags);
+				JSONArray tags = new JSONArray();
+				AssetTagLocalServiceUtil.getTags(BlogsEntry.class.getName(),
+						entry.getEntryId()).forEach(tag -> tags.put(tag.getName()));
+				jsonEntry.put("tags",tags);
 
+				try {
+					jsonEntry.put("image",entry.getCoverImageURL(themeDisplay));
+				} catch (PortalException e) {
+					e.printStackTrace();
+				}
 
-			try {
-				jsonEntry.put("image",entry.getCoverImageURL(themeDisplay));
-			} catch (PortalException e) {
-				e.printStackTrace();
+				jsonReturn.append("data",jsonEntry);
 			}
-
-			jsonReturn.append("data",jsonEntry);
 		});
 
-		renderRequest.setAttribute("usedSearchTerm", searchTerm);
-        renderRequest.setAttribute("contentJson", jsonReturn.toString());
+		renderRequest.setAttribute("searchTerm", searchTerm);
+        renderRequest.setAttribute("searchTag",searchTag);
+		renderRequest.setAttribute("page",page);
+		renderRequest.setAttribute("contentJson", jsonReturn.toString());
+
 		super.doView(renderRequest, renderResponse);
 	}
 
 
-	private List<BlogsEntry> getBlogsByTag(ThemeDisplay themeDisplay, String tagName, int start, int end)
-			throws PortalException {
-		 AssetTag assetTag = AssetTagLocalServiceUtil.getTag(themeDisplay.getScopeGroupId(), tagName);
-
-		 AssetEntryQuery query = new AssetEntryQuery();
-		 query.setAnyTagIds(new long[]{assetTag.getTagId()});
-		 query.setClassName(BlogsEntry.class.getName());
-		 query.setStart(start);
-		 query.setEnd(end);
-
-		 List<BlogsEntry> blogEntries = AssetEntryLocalServiceUtil.getEntries(query).stream().map(i -> {
-		 try {
-		 	return BlogsEntryLocalServiceUtil.getBlogsEntry(i.getClassPK());
-		 } catch (PortalException e) {
-		 	e.printStackTrace();
-		 }
-		 	return null;
-		 }).collect(Collectors.toList());
-
-		 return blogEntries;
-	}
-	
-	public List<AssetEntry> getEntriesList(long groupId, String assetListName) throws PortalException {
-        AssetListEntry assetListEntry = AssetListEntryLocalServiceUtil.getAssetListEntry(groupId, assetListName);
-        List<AssetEntry> entriesList = _assetListAssetEntryProvider.getAssetEntries(assetListEntry, 0);
-
-        BlogsEntry aa = BlogsEntryLocalServiceUtil.getBlogsEntry(37507);
-        return entriesList;
-	} 
-
-	public String toJsonString(List<AssetEntry> entries, String language, String searchTerm) throws JSONException, DocumentException, org.json.JSONException {
-		
-		LinkedHashSet<String> setOfCategories = new LinkedHashSet<String>();
-		JSONObject jsonFullData = new JSONObject();	
-		JSONArray faqsData = new JSONArray();
-		
-		for (AssetEntry entry : entries) {
-			
-			JournalArticle journalArticle = JournalArticleLocalServiceUtil.fetchLatestArticle(entry.getClassPK());
-			
-			if(!journalArticle.isExpired() && !journalArticle.isInTrash() && !journalArticle.isDraft()) { 
-				
-				List<AssetCategory> assetCategories = AssetCategoryLocalServiceUtil.getAssetEntryAssetCategories(entry.getEntryId());
-				
-				Document document = SAXReaderUtil.read(journalArticle.getContentByLocale(language));		 
-				String question = getStructureFieldContent("Pregunta", document);	 
-				String answer = getStructureFieldContent("Respuesta", document);	
-				
-				if (searchFilter(searchTerm, question, answer)) {
-					JSONObject jsonAssetFields =  new JSONObject();
-					List<String> assetCategoryNames = new ArrayList<String>();
-	
-					for (AssetCategory category:assetCategories) {
-						setOfCategories.add(category.getTitle(language));
-						assetCategoryNames.add(category.getTitle(language));
-					}
-					jsonAssetFields.put("question", question);
-					jsonAssetFields.put("answer", answer);
-					jsonAssetFields.put("categories", assetCategoryNames);	
-					jsonFullData.append("data", jsonAssetFields);
-					faqsData.put(jsonAssetFields);
-				 }
-			}
+	private List<BlogsEntry> getBlogsByTag(ThemeDisplay themeDisplay, String tagName, int start, int end) {
+		List<BlogsEntry> blogsEntries = new ArrayList<>();
+		AssetTag assetTag;
+		try {
+			assetTag = AssetTagLocalServiceUtil.getTag(themeDisplay.getScopeGroupId(), tagName);
+		} catch (PortalException e) {
+			return blogsEntries;
 		}
-		jsonFullData.put("foundCategories", setOfCategories);
-		jsonFullData.put("data",faqsData);
-		return jsonFullData.toString();
+
+		AssetEntryQuery query = new AssetEntryQuery();
+		query.setAnyTagIds(new long[]{assetTag.getTagId()});
+		query.setClassName(BlogsEntry.class.getName());
+		query.setStart(start);
+		query.setEnd(end);
+
+		return AssetEntryLocalServiceUtil.getEntries(query).stream().map(i -> {
+			try {
+				return BlogsEntryLocalServiceUtil.getBlogsEntry(i.getClassPK());
+			} catch (PortalException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}).collect(Collectors.toList());
+
 	}
-	
-	public String getStructureFieldContent(String fieldName, Document document) {
-		Node questionNode = document.selectSingleNode("/root/dynamic-element[@name='" + fieldName + "']/dynamic-content");
-		String content = questionNode.getText();
-		return content;
-	}
-	
-	public boolean searchFilter(String searchTerm, String question, String answer) {
-		return searchTerm.isEmpty() 
-				||question.toLowerCase().contains(searchTerm.toLowerCase()) 
-			    || answer.toLowerCase().contains(searchTerm.toLowerCase());
+
+	private List<BlogsEntry> getBlogsBySearch(ThemeDisplay themeDisplay, String searchTerm) {
+		List<BlogsEntry> blogsEntries = new ArrayList<>();
+		FuzzyQuery contentQuery = queries.fuzzy("content",searchTerm);
+		BooleanQuery booleanQuery = queries.booleanQuery().addMustQueryClauses(contentQuery);
+
+		SearchRequestBuilder searchRequestBuilder =	searchRequestBuilderFactory.builder();
+		searchRequestBuilder.emptySearchEnabled(true);
+		searchRequestBuilder.withSearchContext(
+				searchContext -> {
+					searchContext.setCompanyId(themeDisplay.getCompanyId());
+					searchContext.setKeywords(searchTerm);
+				});
+
+		SearchRequest searchRequest = searchRequestBuilder.query(booleanQuery).build();
+		SearchResponse searchResponse = searcher.search(searchRequest);
+
+		searchResponse.getSearchHits().getSearchHits().forEach(searchHit -> {
+			Document doc = searchHit.getDocument();
+			if (doc.getString("entryClassName").equals("com.liferay.blogs.model.BlogsEntry")) {
+				try {
+					blogsEntries.add(BlogsEntryLocalServiceUtil.getBlogsEntry(doc.getInteger("entryClassPK")));
+				} catch (PortalException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		return blogsEntries;
 	}
 
 }
