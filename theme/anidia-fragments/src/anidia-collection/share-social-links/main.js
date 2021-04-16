a388fb6a4ef2e3b688794a135f4ef496f2df8365
Clara Morrondo <claramorrondo@PX0856.local>
2021-04-16 12:21:52 +0200

    general design issues and button added in social share fragment

--- theme/anidia-fragments/src/anidia-collection/share-social-links/main.js
+++ theme/anidia-fragments/src/anidia-collection/share-social-links/main.js
@@ -1,3 +1,14 @@
-console.group('share-social-links');
-console.log('fragmentElement', fragmentElement);
-console.groupEnd();
+function socialShare() {
+  const blogType = document.querySelector(".anidia-single-post-page .component-html");
+  if (blogType) {
+    blogType.classList.add('hide');
+    const text = blogType.innerText.trim();
+    let button = document.querySelector(".share-social-links .an-btn")
+    text === 'gas' ? button.innerHTML = "Descubre Anidia Gas" : button.innerHTML = "Descubre Anidia Solar"
+    text === 'gas' ? button.href = window.location.origin + "/es/web/guest/gas" : button.href = window.location.origin + "/es/web/guest/solar"
+  }
+}
+
+if (document.querySelector(".share-social-links")) {
+  socialShare();
+}
